fa32952fdc91891951ab3c83e7c39e2d27a17702
Karfeinn <kayn.akabane@gmail.com>
2025-12-06 18:38:59 +0100

    Part 2

--- aoc5.py
+++ aoc5.py
@@ -1,28 +1,33 @@
 def read_data():
     with open("input.txt", mode="r", encoding="utf-8") as file:
-        ranges, ids = file.read().split("\n\n", maxsplit=1)
+        ranges, ids= file.read().split("\n\n", maxsplit=1)
     ranges = ranges.split()
-    ids = ids.split()
-    return ids, ranges
+    return ranges
 
-def count_fresh(ids, ranges):
-    count = 0
+def merge_ranges(ranges):
+    ranges = sorted(ranges)
+    merged = [ranges[0]]
 
-    for ingredient in ids:
-        x = int(ingredient)
-        for start, end in ranges:
-            if start <= x <= end:
-                count += 1
-                break
+    for start, end in ranges[1:]:
+        last_start, last_end = merged[-1]
 
-    return count
+        if start <= last_end + 1:
+            merged[-1] = (last_start, max(last_end, end))
+        else:
+            merged.append((start, end))
+
+    return merged
+
+def count_ids(ranges):
+    return sum(end - start + 1 for start, end in ranges)
 
 def function():
-    print("Etape 1")
-    ids, ranges = read_data()
+    ranges = read_data()
     ranges = [tuple(map(int, r.split("-"))) for r in ranges]
-    print("Etape 2")
-    total = count_fresh(ids, ranges)
-    print(total)
+
+    merged = merge_ranges(ranges)
+
+    total_ids = count_ids(merged)
+    print(total_ids)
 
 function()
